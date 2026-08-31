4b54e40367d0742f792339c79323fd1ea5a26b38
Arthur GOMEZ <arthur.gomez.alarcon@gmail.com>
2026-08-31 16:29:01 +0200

    feat(api): update security configuration for stateless authentication
    * configure `SecurityFilterChain` to use stateless session policy
    * disable CSRF for simplified API authentication
    * allow unauthenticated access to `/auth/login` and `/auth/register` endpoints
    * require authentication for all other requests

--- gomoku-core-api/src/main/java/com/gomoku/coreapi/configuration/SecurityConfiguration.java
+++ gomoku-core-api/src/main/java/com/gomoku/coreapi/configuration/SecurityConfiguration.java
@@ -2,8 +2,12 @@ package com.gomoku.coreapi.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfiguration {
@@ -11,5 +15,31 @@ public class SecurityConfiguration {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(
+            final HttpSecurity http
+    ) throws Exception {
+
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(
+                                SessionCreationPolicy.STATELESS
+                        )
+                )
+
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(
+                                "/auth/login",
+                                "/auth/register"
+                        ).permitAll()
+
+                        .anyRequest().authenticated()
+                );
+
+        return http.build();
     }
 }
