c9be5988b738dc193c16672a9e41321592319395
Arthur GOMEZ <arthur.gomez.alarcon@gmail.com>
2026-09-01 17:26:15 +0200

    refactor(api): simplify `MoveModel` by using Lombok annotations
    * replace custom constructors, equals, and hashCode methods with `@AllArgsConstructor`, `@NoArgsConstructor`, and `@EqualsAndHashCode`
    * remove redundant getter methods with `@Getter` annotation

--- gomoku-core-api/src/main/java/com/gomoku/coreapi/model/MoveModel.java
+++ gomoku-core-api/src/main/java/com/gomoku/coreapi/model/MoveModel.java
@@ -1,51 +1,19 @@
 package com.gomoku.coreapi.model;
 
 import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
-
-import java.util.Objects;
+import lombok.NoArgsConstructor;
 
 @Embeddable
 @Getter
+@EqualsAndHashCode
+@NoArgsConstructor(access = lombok.AccessLevel.PROTECTED)
+@AllArgsConstructor
 public class MoveModel {
 
     private Integer x;
 
     private Integer y;
-
-    protected MoveModel() {
-        // JPA
-    }
-
-    public MoveModel(final Integer x, final Integer y) {
-        this.x = x;
-        this.y = y;
-    }
-
-    public Integer getX() {
-        return x;
-    }
-
-    public Integer getY() {
-        return y;
-    }
-
-    @Override
-    public boolean equals(final Object object) {
-        if (this == object) {
-            return true;
-        }
-
-        if (!(object instanceof MoveModel move)) {
-            return false;
-        }
-
-        return Objects.equals(x, move.x)
-                && Objects.equals(y, move.y);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(x, y);
-    }
 }
